9a542cacb6dec3da2370de7e9a362c19e8f06006
Dulaj Kavinda <dulajkaavinda@gmail.com>
2021-02-03 11:27:57 +0530

    example ui changes

--- example/src/App.js
+++ example/src/App.js
@@ -7,13 +7,38 @@ const App = () => {
   const saveFeedback = () => {
     console.log(text)
   }
+
+  const openInNewTab = (url) => {
+    const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
+    if (newWindow) newWindow.opener = null
+  }
   const [text, settext] = useState({ feedback: '' })
   return (
-    <MinimalFeedback
-      save={saveFeedback}
-      value={text}
-      onChange={(e) => settext(e)}
-    />
+    <div
+      style={{
+        display: 'flex',
+        height: '100vh',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column'
+      }}
+    >
+      <h1 style={{ fontFamily: 'revert' }}>minimal-feedback</h1>
+      <p style={{ fontWeight: 'bold' }}>
+        By{' '}
+        <span
+          onClick={() => openInNewTab(`https://github.com/dulajkavinda`)}
+          style={{ color: 'blueviolet', fontWeight: 'bold', cursor: 'pointer' }}
+        >
+          Dulaj Kavinda
+        </span>
+      </p>
+      <MinimalFeedback
+        save={saveFeedback}
+        value={text}
+        onChange={(e) => settext(e)}
+      />
+    </div>
   )
 }
 
